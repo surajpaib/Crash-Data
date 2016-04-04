72b64f9ce9096391dc234181430918279f3e7ea9
surajpaib <surajballambat@gmail.com>
2016-04-05 04:30:02 +0530

    commit2

--- analyze.py
+++ analyze.py
@@ -10,39 +10,78 @@ location=[]
 location_data=[]
 d=[]
 
-ofile=open('AnalyzedData2.csv','wb')
+ofile=open('AnalyzedData.csv','wb')
 writer=csv.writer(ofile,delimiter=',')
+
+cfile=open('states.csv','rb')
+array1=csv.reader(cfile)
+
+for i in array1:
+	location.append(i[0])
+
+
+
+
+
 
 
 for row in reader:
 
 	location_data=row[2]
 	d=location_data.split(",")
+
+
+	for j in location:
+
+		if (len(d)==3):
+			if (d[2].strip()==str(j)):
+				row[2]='US'
+		if (len(d)==2):
+			if (d[1].strip()==str(j)):
+				row[2]='US'
+
+		if (len(d)==1):
+			if (d[0].strip()==str(j)):
+				row[2]='US'
+
+
+
+
+
+
+
+
+
+
+	### To map countries to country codes
 	if (len(d)==3):
-		row[2]=mapping.get(d[2].strip(),'US')
+		if(row[2]!='US'):
+			row[2]=mapping.get(d[2].strip())
 	if (len(d)==2):
-		row[2]=mapping.get(d[1].strip(),'US')
+		if(row[2]!='US'):
+			row[2]=mapping.get(d[1].strip())
 	else:
-		row[2]=mapping.get(d[0].strip(),'US')
+		if(row[2]!='US'):
+			row[2]=mapping.get(d[0].strip())
 
 
 	### To analyze the summary and find estimated reasons
 
 
 	if re.search(r'(.*)[cC]ontrol(.*)',row[6]):
-		row[6]='Control'
+		row[6]='Loss of control'
 	if re.search(r'(.*)[Ll]anding(.*)',row[6]):
-		row[6]='Landing'
+		row[6]='Crashes during Landing and Take-off'
 	if re.search(r'(.*)[lL]ightning(.*)',row[6]):
-		row[6]='Lightning'
+		row[6]='Weather conditions'
 	if re.search(r'(.*)[sS]hot(.*)',row[6]):
-		row[6]='Shot Down'
+		row[6]='Aircraft shot down'
 	if re.search(r'(.*)[hH]ijack(.*)',row[6]):
 		row[6]='Terrorist Acts'
 	if re.search(r'(.*)[bB]omb(.*)',row[6]):
 		row[6]='Terrorist Acts'
 	if re.search(r'(.*)[mM]echanical(.*)',row[6]):
-		row[6]='Mechanical Failure'
+		row[6]='Mechanical component Failure'
 	if re.search(r'(.*)[eE]ngine(.*)',row[6]):
 		row[6]='Engine Failure'
 	if re.search(r'(.*)[pP]ilot(.*)',row[6]):
@@ -68,7 +107,14 @@ for row in reader:
 	if re.search(r'(.*)[dD]isappear(.*)',row[6]):
 		row[6]='Disappearances'
 	if re.search(r'(.*)[cC]rash(.*)',row[6]):
-		row[6]='Accident'
+		row[6]='Accidental Crashes'
+	if re.search(r'(.*)[aA]ccident(.*)',row[6]):
+		row[6]='Accidental Crashes'
+	if re.search(r'(.*)[eE]xplode(.*)',row[6]):
+		row[6]='Explosions on board'
+	if re.search(r'(.*)[sS]truck(.*)',row[6]):
+		row[6]='Accidental Crashes'
+
 
 
 
